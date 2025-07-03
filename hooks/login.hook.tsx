14395ce2d5f40084b6501535efa9c6585502650f
Vinicius <vavizvini000@gmail.com>
2025-07-03 00:41:26 -0300

    feat: adiciona login básico de usuário

---
+++ hooks/login.hook.tsx
@@ -0,0 +1,18 @@
+﻿import { useAxios } from "@/hooks/axios.hook";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+
+interface IProps {
+  email: string;
+  password: string;
+}
+
+export function useLoginUser<R, E>() {
+  const axios = useAxios();
+
+  return useMutation<AxiosResponse<R, any>, AxiosError<E>, IProps, unknown>({
+    mutationFn: async (props: IProps) => {
+      return await axios.post("/user/login", props);
+    },
+  });
+}
